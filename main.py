c171e180cc6ded2780be5a64fdd94c4ed4f485d1
Kislaya Karnik <85802450+KislayaKarnik@users.noreply.github.com>
2022-04-29 00:29:42 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+import requests 
+from bs4 import BeautifulSoup
+import pandas as pd
+
+def get_data(url):
+        res = requests.get(url)
+        soup = BeautifulSoup(res.text,"lxml")
+        books = soup.find_all("article", class_="product_pod")
+        
+        data = []
+        
+        for book in books:
+                item = {}
+                item["Title"] = book.find("img", class_="thumbnail").attrs["alt"]
+                item["Price"] = book.find("p", class_="price_color").text[1:]
+                data.append(item)
+                
+        return data
+
+def export_data(data):
+        df = pd.DataFrame(data)
+        df.to_excel("books.xlsx")
+        
+if __name__ == '__main__':
+        data = get_data("https://books.toscrape.com")
+        export_data(data)
+        print("Done")
